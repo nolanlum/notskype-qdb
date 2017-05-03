3c54d7ca44d0d072f74d6cf50a8f974032781061
Jeffrey Tao <jatao88@gmail.com>
2017-05-03 14:49:51 -0700

    Webpack server, hook up API to interface (#3)
    * Webpack server, hook up API to interface
    
    * revert classifier regex changes
    
    * for real this time
    
    * address PR issues, posting structured data
    
    * merge master webpack config into client webpack config

--- src/components/quote.tsx
+++ src/components/quote.tsx
@@ -6,7 +6,9 @@ require("../../style/quote.scss");
 const Quote = ({id, author, body, addedAt}) =>
     <article class="quote">
         <section class="quote-body">
-            <p class="quote-text">{body}</p>
+            <pre class="quote-text">
+                {JSON.stringify(body, null, 2)}
+            </pre>
         </section>
         <aside class="quote-timestamp">added by {author} {moment(new Date(addedAt)).fromNow()}</aside>
     </article>;
