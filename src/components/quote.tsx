a4e46b5d792f79d35f2714ac67ee6ce9e6e5bc89
Max <mhuan13@gmail.com>
2017-05-05 02:46:21 -0400

    Render quotes on the frontend (#7)
    * support CRLF
    
    * add support for multiline discord logs
    
    * remove compact slack log test
    
    * render parseable quotes as a different format
    
    * fix quote rendering to hooked up app
    
    * remove captured changes from PR
    
    * indicate quote source with border

--- src/components/quote.tsx
+++ src/components/quote.tsx
@@ -1,16 +1,60 @@
 import Component from "inferno-component";
 import * as moment from "moment";
 
+import classifyQuote from "../lib/classifyquote";
+
 require("../../style/quote.scss");
 
-const Quote = ({id, author, body, addedAt}) =>
-    <article class="quote">
-        <section class="quote-body">
-            <pre class="quote-text">
-                {JSON.stringify(body, null, 2)}
-            </pre>
+const QuoteSegment = ({speaker, displayIcon, body}) => {
+    let showUserInfo = speaker || displayIcon;
+    return (
+        <section class={showUserInfo ? "quote-segment" : "quote-segment follow"}>
+            {showUserInfo ?
+                [
+                    <img class="quote-segment-icon" src={displayIcon}/>,
+                    <em class="quote-segment-speaker">{speaker}</em>
+                ]
+                : null}
+            <p class="quote-segment-body">{body}</p>
         </section>
-        <aside class="quote-timestamp">added by {author} {moment(new Date(addedAt)).fromNow()}</aside>
-    </article>;
+        );
+};
+
+const Quote = ({id, author, body, addedAt}) => {
+    let bodyElements;
+    if (body.type === "unrecognized") {
+        bodyElements = body.message;
+    } else {
+        bodyElements = [];
+        let previousSpeaker = undefined;
+        for (let message of body.messages) {
+            if (previousSpeaker === message.speaker) {
+                bodyElements.push(
+                    <QuoteSegment
+                        body={message.body}/>
+                );
+            } else {
+                bodyElements.push(
+                    <QuoteSegment
+                        displayIcon={"https://placehold.it/64x64"}
+                        speaker={message.speaker}
+                        body={message.body}/>
+                );
+            }
+            previousSpeaker = message.speaker;
+        }
+    }
+
+    let quoteClass = body.type;
+
+    return (
+        <article class={"quote " + quoteClass}>
+            <section class="quote-body">
+                <p class="quote-text">{bodyElements}</p>
+            </section>
+            <aside class="quote-timestamp">added by {author} {moment(new Date(addedAt)).fromNow()}</aside>
+        </article>
+    );
+};
 
 export default Quote;
