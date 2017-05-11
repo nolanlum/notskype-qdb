abd4024c0fc415ca7dc16ea49cf707bc73f1af00
Nolan Lum <nolanlum@users.noreply.github.com>
2017-05-11 14:25:14 -0700

    Add actual permalinking, visual tweaks, random quote support. (#14)
    * Add actual permalinking, visual tweaks, random quote support.
    
    When viewing a Quote permalink, there is no longer any loading text. This also applies to the random quote page.
    
    Changed the time submitted display to be more Facebook-y, and converted it to a permalink.
    
    Also cleaned up the random /api/ folder that is not the real API.
    
    * Revert whitespace changes and proxy URL.

--- src/routes.tsx
+++ src/routes.tsx
@@ -4,10 +4,12 @@ import Component from "inferno-component";
 import ApplicationFrame from "./containers/applicationframe";
 import Main from "./containers/main";
 import PermalinkQuote from "./containers/permalinkquote";
+import RandomQuote from "./containers/randomquote";
 import Search from "./containers/search";
 
 export default <Route component={ ApplicationFrame }>
                     <IndexRoute component={ Main }/>
                     <Route path="/quote/:id" component={ PermalinkQuote } />
+                    <Route path="/rand" component={ RandomQuote } />
                     <Route path="/search/:query" component={ Search } />
                 </Route>;
