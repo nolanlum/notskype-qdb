d47b9254302ef2753e1936c944b59f973e6f71d0
Maxwell Huang-Hobbs <mhuan13@gmail.com>
2017-05-11 13:29:35 -0400

    add errorpage (404)

--- src/routes.tsx
+++ src/routes.tsx
@@ -4,10 +4,16 @@ import Component from "inferno-component";
 import ApplicationFrame from "./containers/applicationframe";
 import Main from "./containers/main";
 import PermalinkQuote from "./containers/permalinkquote";
+import ErrorPage from "./containers/errorpage";
 import Search from "./containers/search";
 
 export default <Route component={ ApplicationFrame }>
                     <IndexRoute component={ Main }/>
                     <Route path="/quote/:id" component={ PermalinkQuote } />
                     <Route path="/search/:query" component={ Search } />
+                    <Route path="*"
+                           component={ ErrorPage }
+                           errorCode={404}
+                           errorMessage={"Page not found."}
+                           />
                 </Route>;
