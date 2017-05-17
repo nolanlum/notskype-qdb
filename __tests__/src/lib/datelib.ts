eb4726c78b428f96d6a1924e04b3eea100792002
Nolan Lum <nolanlum@users.noreply.github.com>
2017-05-17 01:34:05 -0700

    why are javascript months zero indexed even in the constructor.

--- __tests__/src/lib/datelib.ts
+++ __tests__/src/lib/datelib.ts
@@ -1,73 +1,75 @@
 import {wasYesterday} from "lib/datelib";
 
+const newHumanDate = (year, month, day) => newHumanDate(year, month - 1, day);
+
 
 describe("wasYesterday", function() {
     it("should decide 5/21 came before 5/22", function() {
-        let today = new Date(2017, 5, 22);
-        let yesterday = new Date(2017, 5, 21);
+        let today = newHumanDate(2017, 5, 22);
+        let yesterday = newHumanDate(2017, 5, 21);
         
         expect(wasYesterday(yesterday, today)).toEqual(true);
     });
     
     it("should decide 5/22 did not come before 5/21", function() {
-        let today = new Date(2017, 5, 22);
-        let yesterday = new Date(2017, 5, 21);
+        let today = newHumanDate(2017, 5, 22);
+        let yesterday = newHumanDate(2017, 5, 21);
         
         expect(wasYesterday(today, yesterday)).toEqual(false);
     });
     
     it("should decide 4/30 came before 5/1", function() {
-        let today = new Date(2017, 5, 1);
-        let yesterday = new Date(2017, 4, 30);
+        let today = newHumanDate(2017, 5, 1);
+        let yesterday = newHumanDate(2017, 4, 30);
         
         expect(wasYesterday(yesterday, todayy)).toEqual(true);
     });
     
     it("should decide 7/31 came before 8/1", function() {
-        let today = new Date(2017, 8, 1);
-        let yesterday = new Date(2017, 7, 31);
+        let today = newHumanDate(2017, 8, 1);
+        let yesterday = newHumanDate(2017, 7, 31);
         
         expect(wasYesterday(yesterday, today)).toEqual(true);
     });
     
     it("should decide 8/31 came before 9/1", function() {
-        let today = new Date(2017, 9, 1);
-        let yesterday = new Date(2017, 8, 31);
+        let today = newHumanDate(2017, 9, 1);
+        let yesterday = newHumanDate(2017, 8, 31);
         
         expect(wasYesterday(yesterday, today)).toEqual(true);
     });
     
     it("should decide 2/28/2017 came before 3/1/2017", function() {
-        let today = new Date(2017, 3, 1);
-        let yesterday = new Date(2017, 2, 28);
+        let today = newHumanDate(2017, 3, 1);
+        let yesterday = newHumanDate(2017, 2, 28);
         
         expect(wasYesterday(yesterday, today)).toEqual(true);
     });
     
     it("should decide 2/28/2016 did not come before 3/1/2016", function() {
-        let today = new Date(2016, 3, 1);
-        let yesterday = new Date(2016, 2, 28);
+        let today = newHumanDate(2016, 3, 1);
+        let yesterday = newHumanDate(2016, 2, 28);
         
         expect(wasYesterday(yesterday, today)).toEqual(false);
     });
     
     it("should decide 2/29/2016 came before 3/1/2016", function() {
-        let today = new Date(2016, 3, 1);
-        let yesterday = new Date(2016, 2, 29);
+        let today = newHumanDate(2016, 3, 1);
+        let yesterday = newHumanDate(2016, 2, 29);
         
         expect(wasYesterday(yesterday, today)).toEqual(true);
     });
     
     it("should decide 2/29/2000 came before 3/1/2000", function() {
-        let today = new Date(2000, 3, 1);
-        let yesterday = new Date(2000, 2, 29);
+        let today = newHumanDate(2000, 3, 1);
+        let yesterday = newHumanDate(2000, 2, 29);
         
         expect(wasYesterday(yesterday, today)).toEqual(true);
     });
     
     it("should decide 12/31/2016 came before 1/1/2017", function() {
-        let today = new Date(2017, 1, 1);
-        let yesterday = new Date(2016, 12, 31);
+        let today = newHumanDate(2017, 1, 1);
+        let yesterday = newHumanDate(2016, 12, 31);
         
         expect(wasYesterday(yesterday, today)).toEqual(true);
     });
